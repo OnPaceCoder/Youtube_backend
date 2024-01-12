19f2a2c8b043661b1e7db5d05cb06e0dd3c59f0f
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-12 21:48:24 +0530

    Add: Cloudinary utility created

---
+++ utils/cloudinary.js
@@ -0,0 +1,50 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { ApiError } from "./ApiError.js";
+
+const deleteOnCloudinary = async (publicId) => {
+    try {
+
+
+
+        const result = await cloudinary.uploader.destroy(publicId);
+
+        return result;
+    } catch (error) {
+        throw new ApiError(400, "Not able to delete old image")
+    }
+}
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        });
+
+
+        if (!localFilePath) return null;
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+
+        fs.unlinkSync(localFilePath);
+        return response;
+
+    } catch (error) {
+
+        fs.unlinkSync(localFilePath); //Remove the locally saved temp file
+        return null;
+    }
+
+
+
+}
+
+export { uploadOnCloudinary, deleteOnCloudinary }
