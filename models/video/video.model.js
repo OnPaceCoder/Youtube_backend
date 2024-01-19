9807a2e5753697dbd3f557aa14d191198735d3bf
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-19 21:38:28 +0530

    fix: Thumbnail public Id added to model

--- models/video/video.model.js
+++ models/video/video.model.js
@@ -35,6 +35,10 @@ const videoSchema = new Schema({
         type: String,
         required: true
     },
+    thumbnailPublicId: {
+        type: String,
+        required: true
+    },
     owner: {
         type: Schema.Types.ObjectId,
         ref: "User"
