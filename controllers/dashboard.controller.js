15944dac00175e4eaa66a2baaa2791320d1b12e7
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-01-22 23:31:13 +0530

    Add: Dashboard controller created

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,3 +1,4 @@
+import { Subscription } from "../models/video/subscription.model.js";
 import { Video } from "../models/video/video.model.js"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -6,6 +7,98 @@ const getChannelStats = async (req, res, next) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
     try {
+
+        const channelStats = await Video.aggregate([
+            {
+                $match: {
+                    owner: req.user._id
+                }
+            },
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "video",
+                    as: "likedBy",
+                    pipeline: [
+                        {
+                            $project: {
+                                likedBy: 1,
+                                _id: 0
+                            }
+                        }
+                    ]
+
+                }
+            },
+
+            {
+                $group: {
+                    _id: null,
+                    totalVideos: { $sum: 1 },
+                    totalViewsOfAllVideos: { $sum: "$views" },
+                    totalLikes: {
+                        $sum: { $size: "$likedBy" }
+                    },
+
+
+                }
+            },
+            {
+                $project: {
+                    totalVideos: 1,
+                    totalViewsOfAllVideos: 1,
+                    totalLikes: 1,
+
+                }
+            }
+
+        ])
+
+
+        const totalSubscribers = await Subscription.aggregate([
+            {
+                $match: {
+                    channel: req.user._id
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalSubscribers: {
+                        $sum: 1
+                    }
+                }
+            },
+            {
+                $project: {
+                    totalSubscribers: 1,
+                    _id: 0
+                }
+            }
+        ])
+
+
+        channelStats[0] = { ...channelStats[0], ...totalSubscribers[0] }
+
+
+
+
+        if (!totalSubscribers) {
+            throw new ApiError(404, "Error occured while counting subscribers")
+        }
+
+
+
+        if (!channelStats) {
+            throw new ApiError(404, "Videos not found")
+        }
+
+
+        return res.status(200).json(new ApiResponse(200, channelStats, "Channel videos fetched successfully"))
+
+
+
 
 
     } catch (error) {
