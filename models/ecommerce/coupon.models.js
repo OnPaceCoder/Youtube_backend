4568c1f4a454a8f4d8342d44233178f2e8e419f1
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-02 23:42:27 +0530

    Add: Coupon model created

---
+++ models/ecommerce/coupon.models.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+const couponSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+
+        couponCode: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            uppercase: true,
+        },
+        type: {
+            type: String,
+            enum: AvailableCouponTypes,
+            default: CouponTypeEnum.FLAT
+        },
+        discountedValue: {
+            type: Number,
+            required: true
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        },
+        minimumCartValue: {
+            type: Number,
+            default: 0,
+        },
+        startDate: {
+            type: Date,
+            default: Date.now,
+        },
+        expiryDate: {
+            type: Date,
+            default: null
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+
+
+    },
+    { timestamps: true }
+)
+
+export const Coupon = mongoose.model("Coupon", couponSchema)
