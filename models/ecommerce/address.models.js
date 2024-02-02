5b343864e1e84f4c7718c4d126581c9d2b694f43
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-02 23:41:42 +0530

    Add: Address model created

---
+++ models/ecommerce/address.models.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const addressSchema = new Schema({
+    addressLine1: {
+        required: true,
+        type: String,
+    },
+    addressLine2: {
+        type: String,
+    },
+    city: {
+        required: true,
+        type: String
+    },
+    country: {
+        required: true,
+        type: String
+    },
+    owner: {
+        ref: "User",
+        type: Schema.Types.ObjectId
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    state: {
+        required: true,
+        type: String
+    }
+},
+    {
+        timestamps: true
+    })
+
+export const Address = mongoose.model("Address", addressSchema)
