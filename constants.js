e50a3f92ffdc0b503b79b6f63c49b9e14aaa0289
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-02 23:40:17 +0530

    Add: Constant file updated

--- constants.js
+++ constants.js
@@ -1,1 +1,38 @@
 export const DB_NAME = "videotube"
+
+
+// User Roles
+
+export const UserRolesEnum = {
+    ADMIN: "ADMIN",
+    USER: "USER"
+};
+export const AvailableUserRoles = Object.values(UserRolesEnum)
+
+// Order Enum
+
+export const OrderStatusEnum = {
+    PENDING: "PENDING",
+    CANCELLED: "CANCELLED",
+    DELIVERED: "DELIVERED"
+}
+
+export const AvailableOrderStatuses = Object.values(OrderStatusEnum);
+
+
+// Payment Provider Enum
+export const PaymentProviderEnum = {
+    UNKNOWN: "UNKNOWN",
+    RAZORPAY: "RAZORPAY",
+    PAYPAL: "PAYPAL",
+};
+
+export const AvailablePaymentProviders = Object.values(PaymentProviderEnum);
+
+// Coupon Types Enum
+
+export const CouponTypeEnum = {
+    FLAT: "FLAT"
+}
+
+export const AvailableCouponTypes = Object.values(AvailableCouponTypes)
