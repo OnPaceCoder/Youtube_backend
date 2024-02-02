ae652d99ca31ecc2b5d886d31c7474304a667e2c
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-02 23:57:54 +0530

    Add: Validate file created

---
+++ validators/validate.js
@@ -0,0 +1,16 @@
+import { validateResult } from "express-validator"
+import { ApiError } from '../utils/ApiError.js'
+export const validate = (req, res, next) => {
+    const errors = validateResult(req);
+
+    if (errors.isEmpty()) {
+        return next()
+    }
+
+    const extractedErrors = [];
+
+    errors.array().map((err) => extractedErrors.push({ [err.path]: [err.msg] }));
+
+    //422: Unprocessable Entity
+    throw new ApiError(422, "Received data is not valid", extractedErrors)
+}
