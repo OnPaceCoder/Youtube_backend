00f17c013d7a63e537d0355075b33d0e80d093fa
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-04 22:42:22 +0530

    Refact: mongoose aggregate plugin added

--- models/ecommerce/category.models.js
+++ models/ecommerce/category.models.js
@@ -1,5 +1,6 @@
 
 import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const categorySchema = new mongoose.Schema({
     name: {
@@ -13,5 +14,6 @@ const categorySchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
+categorySchema.plugin(mongooseAggregatePaginate)
 
 export const Category = mongoose.model("Category", categorySchema)
