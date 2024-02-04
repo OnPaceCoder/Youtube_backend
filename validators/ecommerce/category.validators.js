1dc531eca2dae5800e4e57a43397a394d1c15ea2
Priyank Vasoya <priyankvasoya7@gmail.com>
2024-02-04 22:43:33 +0530

    Add: Category validator created

---
+++ validators/ecommerce/category.validators.js
@@ -0,0 +1,8 @@
+import { body, param } from "express-validator";
+const categoryRequestBodyValidator = () => {
+    return [
+        body("name").trim().notEmpty().withMessage("Category name is required")
+    ]
+}
+
+export { categoryRequestBodyValidator }
